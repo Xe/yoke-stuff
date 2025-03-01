353eef1ee6ee2cddfaa4ab86fcddcf8c5c3e23c1
Xe Iaso <me@xeiaso.net>
2025-03-01 12:59:05 -0500

    use presigned URLs
    Signed-off-by: Xe Iaso <me@xeiaso.net>

--- within-website-app/build.sh
+++ within-website-app/build.sh
@@ -4,12 +4,12 @@ set -euo pipefail
 
 export GOOS=wasip1
 export GOARCH=wasm
-export AWS_PROFILE=alrest
+export AWS_PROFILE=tigris
 
 go build -o x-app.wasm ./v1/flight
 go build -o x-app-airway.wasm ./v1/airway
 
 gzip -f *.wasm
 
-aws s3 cp ./x-app.wasm.gz s3://mi-static/yoke/x-app/v1.wasm.gz
-aws s3 cp ./x-app-airway.wasm.gz s3://mi-static/yoke/x-app/airway/v1.wasm.gz
+aws s3 cp ./x-app.wasm.gz s3://xedn/yoke/x-app/v1.wasm.gz
+aws s3 cp ./x-app-airway.wasm.gz s3://xedn/yoke/x-app/airway/v1.wasm.gz
--- within-website-app/deploy.sh
+++ within-website-app/deploy.sh
@@ -2,4 +2,9 @@
 
 set -euo pipefail
 
-yoke takeoff appairway https://minio.xeserv.us/mi-static/yoke/x-app/airway/v1.wasm.gz -- --flight-url="https://minio.xeserv.us/mi-static/yoke/x-app/v1.wasm.gz?cachebuster=$(uuidgen)"
+export AWS_PROFILE=tigris
+
+AIRWAY_URL=$(aws s3 presign s3://xedn/yoke/x-app/airway/v1.wasm.gz)
+FLIGHT_URL=$(aws s3 presign s3://xedn/yoke/x-app/v1.wasm.gz)
+
+yoke takeoff appairway $AIRWAY_URL -- --flight-url="$FLIGHT_URL?cachebuster=$(uuidgen)"
