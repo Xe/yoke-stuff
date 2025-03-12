f409193008c23cc014318a2f6c8c50472b71eac2
Xe Iaso <me@xeiaso.net>
2025-03-12 10:52:08 -0400

    use OCI registries
    Signed-off-by: Xe Iaso <me@xeiaso.net>

--- within-website-app/build.sh
+++ within-website-app/build.sh
@@ -9,7 +9,5 @@ export AWS_PROFILE=tigris
 go build -o x-app.wasm ./v1/flight
 go build -o x-app-airway.wasm ./v1/airway
 
-gzip -f *.wasm
-
-aws s3 cp ./x-app.wasm.gz s3://xedn/yoke/x-app/v1.wasm.gz
-aws s3 cp ./x-app-airway.wasm.gz s3://xedn/yoke/x-app/airway/v1.wasm.gz
+yoke stow ./x-app.wasm oci://registry.int.xeserv.us/x-app/flight:v1
+yoke stow ./x-app-airway.wasm oci://registry.int.xeserv.us/x-app/airway:v1
--- within-website-app/deploy.sh
+++ within-website-app/deploy.sh
@@ -2,9 +2,4 @@
 
 set -euo pipefail
 
-export AWS_PROFILE=tigris
-
-AIRWAY_URL=$(aws s3 presign s3://xedn/yoke/x-app/airway/v1.wasm.gz)
-FLIGHT_URL=$(aws s3 presign s3://xedn/yoke/x-app/v1.wasm.gz)
-
-yoke takeoff appairway $AIRWAY_URL -- --flight-url="$FLIGHT_URL"
+yoke takeoff appairway oci://reg.xeiaso.net/x-app/airway:v1 -- --flight-url=oci://reg.xeiaso.net/x-app/flight:v1
