8c8b94a7fdafb2001c496f7f6e06129a9f6f6c1a
Xe Iaso <me@xeiaso.net>
2025-05-06 08:26:23 -0400

    chore: add install files
    Signed-off-by: Xe Iaso <me@xeiaso.net>

---
+++ db/valkey/yeetfile.install.js
@@ -0,0 +1,2 @@
+$`go tool yeet`;
+$`yoke takeoff valkeyairway oci://reg.xeiaso.net/x-app/db/valkey/airway:${git.tag()} -- --flight-url=oci://reg.xeiaso.net/x-app/db/valkey/flight:${git.tag()}`;
---
+++ within-website-app/yeetfile.install.js
@@ -0,0 +1,2 @@
+$`go tool yeet`;
+$`yoke takeoff appairway oci://reg.xeiaso.net/x-app/airway:${git.tag()} -- --flight-url=oci://reg.xeiaso.net/x-app/flight:${git.tag()}`;
